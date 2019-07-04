c906d02adea99fae944300780a7a577876458d9a
arthur.portela <arthur.portela.residente@tre-rn.jus.br>
2019-07-04 17:06:34 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+
+r  = requests.get('https://rn.olx.com.br/imoveis/aluguel/apartamentos')
+count = 1;
+while r.status_code == 200:
+
+
+	html_doc = r.text #instancia a variavel html_doc com o HTML recebido
+	#transforma pagina recebida em um obj do bs4 
+	soup = BeautifulSoup(html_doc, 'html.parser') 
+
+	anuncios = soup.findAll('a', {"class": 'OLXad-list-link'})
+	divs_precos = soup.findAll('div', {"class": 'col-3'})
+	descricoes = soup.findAll('p', {"class": 'text detail-specific'})
+
+	titulos = []
+	links = []
+	precos = []
+	detalhes = []
+	data = []
+	dicionario = dict()
+
+	for anuncio in anuncios:
+		titulos.append(anuncio['title'])
+
+	for anuncio in anuncios:
+		links.append(anuncio['href'])
+
+	for divs_preco in divs_precos:
+		precos.append(divs_preco.getText())
+
+	for descricao in descricoes:
+		detalhes.append(descricao.getText())
+
+
+
+	for i in range(0,len(titulos)):
+		dicionario['titulo'] = titulos[i]
+		dicionario['link'] = links[i]
+		dicionario['preco'] = precos[i]
+		dicionario['detalhe'] = detalhes[i]
+		data.append(dicionario)
+		dicionario = {}
+
+
+
+	
+
+	count = count + 1;
+	r = requests.get('https://rn.olx.com.br/imoveis/aluguel/apartamentos?o='+str(count))
+
+
+df = pd.DataFrame(data).dropna()
+
+df['detalhe'] = df['detalhe'].str.replace('([\n\t])', '')
+df['detalhe'] = df['detalhe'].str.replace('(\s{3,})', '')
+
+#print df
+df.to_csv('resultados.csv', encoding = 'latin')
+
+
+
+
